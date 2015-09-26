3911d1544c2c50a21bed9ae3998342e66d1512f4
Adrian <A0122061@u.nus.edu>
2015-09-26 14:14:17 +0800

    Addition of 2 library that stores possible CRUD commands and time input(partially done)

--- Adrian/CommandParser.java
+++ Adrian/CommandParser.java
@@ -1,13 +1,33 @@
 public class CommandParser {
+	String NOT_FOUND = "invalid input";
 	public String input;
+	public String[] inputArr;
 	public CommandPackage inputData;
+	public ActionLibrary actionLib;
 
 	public CommandParser(String command) {
 		input = command;
+		inputArr = input.split(" ");
+		actionLib = new ActionLibrary();
 	}
+
 	public CommandPackage getInput() {
 		return inputData;
 	}
+
+	public String findAction() {
+
+		if (!callAction(inputArr[0]).equals(NOT_FOUND)) {
+			return callAction(inputArr[0]);
+		} else if (!callAction(inputArr[inputArr.length - 1]).equals(NOT_FOUND)) {
+			return callAction(inputArr[inputArr.length - 1]);
+		}
+
+		return "";
+	}
+
+	private String callAction(String string) {
+		String result = actionLib.find(string);
+		return result;
+	}
 }
-
-
