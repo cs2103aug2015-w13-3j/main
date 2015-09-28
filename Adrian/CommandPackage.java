780d0760e2e2403b0eb9ea3616ce145090702e4e
Adrian <A0122061@u.nus.edu>
2015-09-28 11:58:05 +0800

    Test conflict file

--- Adrian/CommandPackage.java
+++ Adrian/CommandPackage.java
@@ -3,6 +3,7 @@ import java.util.Date;
 
 //This program is the output class from the parsing program for Daxuan to use to execute CRUD
 
+//this is a conflict
 public class CommandPackage {
 	private String command;
 	private DatePackage dates;
