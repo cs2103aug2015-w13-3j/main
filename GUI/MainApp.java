0918e96156188fd5c2f1986cb1f18f3fc62915b9
Sumer <susumaibaobao@gmail.com>
2015-10-07 02:15:33 +0800

    Delete package

--- GUI/MainApp.java
+++ GUI/MainApp.java
@@ -5,11 +5,11 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 
 public class MainApp extends Application{
