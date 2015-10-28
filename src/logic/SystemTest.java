3ca129a55ed339667794935b3a3fb76305df4e5a
BaiChuann <buerlanbo2@gmail.com>
2015-10-28 12:58:45 +0800

    V0.3 with systemtest

--- src/logic/SystemTest.java
+++ src/logic/SystemTest.java
@@ -17,13 +17,14 @@ public class SystemTest {
 		CommandPackage cmd = cmp.getCommandPackage("add meeting 9-Oct");
 		LogicClass lg = LogicClass.getInstance(null);
 		lg.executeCommand(cmd);
+		int size = LogicClass.getTaskList().size();
 		assertEquals(LogicClass.getTaskList().get(0).getName(), "meeting");
 		cmd = cmp.getCommandPackage("update `meeting `name `party");
 		lg.executeCommand(cmd);
 		assertEquals(LogicClass.getTaskList().get(0).getName(), "party");
 		cmd = cmp.getCommandPackage("delete party");
 		lg.executeCommand(cmd);
-		assertEquals(LogicClass.getTaskList().size(), 0);
+		assertEquals(LogicClass.getTaskList().size(), size - 1);
 		/*assertEquals(LogicClass.getInstance(null).delete("meeting").getName(), "meeting");
 		cmd = cmp.getCommandPackage("")*/
 	}
