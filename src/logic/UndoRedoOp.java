dea7f7c4d3c080fb7112cf6d36205b59458409d3
Da Xuan <xuan138139@gmail.com>
2015-10-29 12:37:27 +0800

    undo redo v0.4

--- src/logic/UndoRedoOp.java
+++ src/logic/UndoRedoOp.java
@@ -3,6 +3,8 @@ package logic;
 import java.util.ArrayList;
 import java.util.Stack;
 
+import javax.swing.undo.UndoManager;
+
 
 public class UndoRedoOp {
 
@@ -12,15 +14,24 @@ public class UndoRedoOp {
 	
 	public UndoRedoOp(ArrayList<Task> initialState){
 		undoStack = new Stack<ArrayList<Task>>();
+		undoStack.push(initialState);
 		redoStack = new Stack<ArrayList<Task>>();
 		this.initialState = initialState;
 	}
 	
 	public ArrayList<Task> undo(){
+		
 		if(!undoStack.isEmpty()){
 			ArrayList<Task> currentState = undoStack.pop();
+			System.out.println("after pop: "+ undoStack.toString());
 			redoStack.push(currentState);
-			return undoStack.peek();
+			System.out.println("current state: "+ currentState);
+			System.out.println("return state: "+ undoStack.peek());
+			if(!undoStack.isEmpty()){
+				return undoStack.peek();
+			}else{
+				return initialState;
+			}
 		}
 		return initialState;
 	}
@@ -29,13 +40,14 @@ public class UndoRedoOp {
 		if(!redoStack.isEmpty()){
 			ArrayList<Task> previousState = redoStack.pop();
 			undoStack.push(previousState);
-			return redoStack.peek();
+			return undoStack.peek();
 		}
 		return initialState;
 	}
 	
 	public ArrayList<Task> addStateToUndo(ArrayList<Task> recentState){
 		undoStack.push(recentState);
+		System.out.println("after adding state: "+ undoStack.toString());
 		return recentState;
 	}
 }
